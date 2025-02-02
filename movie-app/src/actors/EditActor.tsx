fa3e26125a2d64c6968d5c35381fefab7c01a801
anhnguyen6535 <anhnguyen6535@gmail.com>
2025-02-02 15:31:40 -0700

    add actor forms

--- movie-app/src/actors/EditActor.tsx
+++ movie-app/src/actors/EditActor.tsx
@@ -1,6 +1,11 @@
+import ActorForm from "./ActorForm";
+
 export default function EditActor() {
     return<>
         <h3>Edit Genre</h3>
+        <ActorForm model={{name: 'Tom Holland', dateOfBirth: new Date('1996-06-01T00:00:00')}} 
+            onSubmit={values => console.log(values)}
+        />
         
     </>
 }
