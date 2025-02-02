551326a30e743727cb79c8c8c261338cf8ba5254
anhnguyen6535 <anhnguyen6535@gmail.com>
2025-02-02 15:31:33 -0700

    Update route-config.ts

--- movie-app/src/route-config.ts
+++ movie-app/src/route-config.ts
@@ -20,14 +20,14 @@ const routes = [
 
     {path: '/actors', component: IndexActor},
     {path: '/actors/create', component: CreateActor},
-    {path: '/actors/edit/id', component: EditActor},
+    {path: '/actors/edit/:id', component: EditActor},
 
     {path: '/movietheaters', component: IndexMovieTheaters},
     {path: '/movietheaters/create', component: CreateMovieTheater},
-    {path: '/movietheaters/edit/id', component: EditMovieTheater},
+    {path: '/movietheaters/edit/:id', component: EditMovieTheater},
 
     {path: '/movies/create', component: CreateMovie},
-    {path: '/movies/edit/id', component: EditMovie},
+    {path: '/movies/edit/:id', component: EditMovie},
     {path: '/movies/filter', component: FilterMovies},
     
     {path: '/', component: LandingPage},
