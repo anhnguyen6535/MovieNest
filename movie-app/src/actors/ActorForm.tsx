7806064d6ef85b3b890a0124d941862e1ba54ac2
anhnguyen6535 <anhnguyen6535@gmail.com>
2025-02-02 18:40:57 -0700

    add Actor form

--- movie-app/src/actors/ActorForm.tsx
+++ movie-app/src/actors/ActorForm.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import * as Yup from "yup";
 import DateField from "../forms/DateField";
 import ImageField from "../forms/ImageField";
+import MarkdownField from "../forms/MarkdownField";
 
 export default function ActorForm(props: actorFormProps) {
     return(
@@ -21,6 +22,7 @@ export default function ActorForm(props: actorFormProps) {
                     <TextField displayName="Name" field="name" />
                     <DateField displayName="Date of Birth" field="dateOfBirth"/>
                     <ImageField displayName="Picture" field="picture" imageURL={props.model.pictureURL || ''}/>
+                    <MarkdownField displayName="Biography" field="biography" />
                     <Button disabled={formikProps.isSubmitting} type="submit">Save Changes</Button>
                     <Link to="/actors" className="btn btn-secondary">Cancel</Link>
                 </Form>
--- movie-app/src/actors/EditActor.tsx
+++ movie-app/src/actors/EditActor.tsx
@@ -3,7 +3,12 @@ import ActorForm from "./ActorForm";
 export default function EditActor() {
     return<>
         <h3>Edit Genre</h3>
-        <ActorForm model={{name: 'Tom Holland', dateOfBirth: new Date('1996-06-01T00:00:00'), pictureURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Tom_Holland_by_Gage_Skidmore.jpg/800px-Tom_Holland_by_Gage_Skidmore.jpg'}} 
+        <ActorForm 
+            model={{name: 'Tom Holland', dateOfBirth: new Date('1996-06-01T00:00:00'), 
+                    pictureURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Tom_Holland_by_Gage_Skidmore.jpg/800px-Tom_Holland_by_Gage_Skidmore.jpg',
+                    biography: `# Something
+This person was born in **LR** `,
+                }} 
             onSubmit={values => console.log(values)}
         />
         
